3e580a576ba3022ae01e8c445558cad36009ba3f
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-01-19 11:22:01 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+int main()
+{     // switch
+		char Engineers;
+	printf("Enter your favourite alphabet (A to Z ) ");
+	scanf("%s",&Engineers);
+	switch(Engineers){
+		case 'a':printf(" A for Air and Space Engineers");
+		break;
+		case 'b':printf(" B for Biomedical Engineers");
+		break;
+		case 'c':printf(" C for Civil Engineers");
+		break;
+		case 'd':printf(" D for Developement Engineers");
+		break;
+		case 'e':printf(" E for Electronics Engineers");
+		break;
+		case 'f':printf(" F for Food Engineers");
+		break;
+		case 'g':printf(" G for Genetic Engineers ");
+		break;
+		case 'h':printf(" H for Highway Engineers");
+		break;
+		case 'i':printf(" I for Insulation Engineers");
+		break;
+		case 'j':printf(" J for Jet Engineers");
+		break;
+		case 'k':printf(" K for Kinetic Engineers");
+		break;
+		case 'l':printf(" L for Lighting Engineer");
+		break;
+		case 'm':printf(" M for Mechanical Engineers");
+		break;
+		case 'n':printf(" N for Nuclear Engineers");
+		break;
+		case 'o':printf(" O for Oil Engineers");
+		break;
+		case 'p':printf(" P for Paper Engineers");
+		break;
+		case 'q':printf(" Q for Quality Engineers");
+		break;
+		case 'r':printf(" R for Railroad Engineers");
+		break;
+		case 's':printf(" S for Strutural Engineers");
+		break;
+		case 't':printf(" T for Technology Engineers");
+		break;
+		case 'u':printf(" U for Utilities Engineers");
+		break;
+		case 'v':printf(" V for Veterinarian Engineers");
+		break;
+
+        case 'w':printf(" W for Water and Sewer Engineers");
+		break;
+		case 'x':printf(" X for X-ray Engineers");
+		break;
+		case 'y':printf(" Y for Yield Engineers");
+		break;
+		case 'z':printf(" Z for Zoologist Enginers");
+		break;
+		default:printf("Not exist Between A to Z");
+
+		
+
+
+}
+   return 0;
+}
